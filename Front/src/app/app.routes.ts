e547ef05e4a61b51c4d29d9f74dedb7229318310
Russo Matias <matias.russo000@gmail.com>
2025-09-16 22:07:47 -0300

    Correción para poner por defecto rutas hacia landing, y acomodé ruta login. Corregí pequeños errores que no dejaban exportar bien los modulos en landing.ts

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login.component/login.component';
+import { LandingComponent } from './componentes/landing/landing';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },   // <- agregada
-  { path: 'login', component: LoginComponent },
-  // opcionalmente, ruta comodín para 404
-  { path: '**', redirectTo: 'landing' }
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },   // Por defecto a la landing
+  { path: 'landing', component: LandingComponent },         // landing 
+  { path: 'login', component: LoginComponent },             // login 
+  { path: '**', redirectTo: 'landing' }                     // cualquier otra ruta va a redirigir a la de defecto
 ];
 
--- Front/src/app/componentes/landing/landing.ts
+++ Front/src/app/componentes/landing/landing.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.html',
-  styleUrl: './landing.css'
+  styleUrls: ['./landing.css']   // corregido, estaba mal nombrado
 })
-export class Landing {
-
-}
+export class LandingComponent { } // Renombré, no coincidian
